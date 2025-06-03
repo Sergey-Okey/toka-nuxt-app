edbc170aa27ba989dcbdd21cee5f7c2c90b72bec
Sergey-Okey <sergeyborisov_1@vk.ru>
2025-06-03 08:25:49 +0300

    migration beta

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,17 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+// nuxt.config.ts
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
+
   css: [
     '@/assets/styles/main.scss',
     '@/assets/styles/base.scss',
     'material-icons/iconfont/material-icons.css',
   ],
+
   components: true,
-  modules: ['@pinia/nuxt', '@primevue/nuxt-module'],
-  build: {},
+
+  modules: ['@pinia/nuxt'],
+
   plugins: [],
 })
